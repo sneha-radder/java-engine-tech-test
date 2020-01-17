0fbc3ae49dfa78cabf451750950650dae7337f6f
sneha-radder <59999027+sneha-radder@users.noreply.github.com>
2020-01-17 18:32:03 +0530

    Update Client.java

--- Client/src/main/java/com/cg/Client/Client.java
+++ Client/src/main/java/com/cg/Client/Client.java
@@ -18,7 +18,10 @@ public class Client {
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setDoOutput(true);
         con.setRequestMethod("POST");
-        byte[] outputInBytes = body.getBytes("UTF-8");
+        byte[] outputInBytes = body.getBytes("UTF-8")
+        String url = "jdbc:mysql://localhost:3306/contactdb";
+        String user = "root";
+        String password = "secret";    
 
         OutputStream os = con.getOutputStream();
         os.write( outputInBytes );    
@@ -28,8 +31,24 @@ public class Client {
         String result = null;
         try (Scanner scanner = new Scanner(in, StandardCharsets.UTF_8.name())) {
             result = scanner.useDelimiter("\\A").next();
+            
+            Connection conn = DriverManager.getConnection(url, user, password);
+ 
+            String sql = "INSERT INTO person (BasicWeight, PercentageChance) values (?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, "85");
+            statement.setString(2, "65");
+ 
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                System.out.println("A value is insered in the row");
+            }
+            conn.close();  
+        }catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-
         in.close();
         con.disconnect();
 
