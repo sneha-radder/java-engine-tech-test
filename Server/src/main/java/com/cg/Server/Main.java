0b184428e5276837327b75023f9ddb87e350bca3
sneha-radder <59999027+sneha-radder@users.noreply.github.com>
2020-01-22 21:27:54 +0530

    Update Main.java

--- Server/src/main/java/com/cg/Server/Main.java
+++ Server/src/main/java/com/cg/Server/Main.java
@@ -13,6 +13,22 @@ public class Main {
                 switch(req.body()) {
                     case "Hello":
                         return hello();
+                    case "url":
+                        Class.forName("com.mysql.jdbc.Driver");
+                         con = DriverManager.getConnection(
+                      "jdbc:mysql://localhost:3306/contactdb","root","secret"); 
+                        prepareStatement statement = con.prepareStatement("SELECT * FROM person WHERE BasicWeight=?");
+                        statement.setInt(1, BasicWeight);
+                        ResultSet rs = statement.executeQuery();
+                        while(rs.next()){
+                            this.BasicWeight = rs.getInt(1);
+                            this.PercentageChance = rs.getString(2); 
+                        }
+                        if(rs!=null)
+                            return rs;
+                        else
+                            return "No values in the table";
+                        
                     //other scenarios could go here ;)
                     default:
                         return "Error! No or invalid request name specified! (" + req.body() + ")";
