3ee86dd422aad8f400e4576e870b96a2e61cb9df
sneha-radder <59999027+sneha-radder@users.noreply.github.com>
2020-01-24 20:09:35 +0530

    Update CommunicationTest.java

--- Client/src/test/java/com/cg/Client/CommunicationTest.java
+++ Client/src/test/java/com/cg/Client/CommunicationTest.java
@@ -16,6 +16,21 @@ public class CommunicationTest {
         String res = Client.postRequest("hello");
         assert (res.equals("Error! No or invalid request name specified! (hello)"));
     }
+    
+    @Test
+    public void testTableAggregateResponses() throws Exception {
+        Connection conn = DriverManager.getConnection(url, user, password);
+        prepareStatement statement = con.prepareStatement("SELECT * FROM persons WHERE BasicWeight=?");
+        statement.setInt(1, BasicWeight);
+        statement.setInt(1, PercentageChance);
+        ResultSet rs = statement.executeQuery();
+        while(rs.next()){
+            assert (res.equals("There is a value in the table with value percentage chance));
+        }
+        if(rs== null){
+           assert (res.equals("Table is empty));
+        }
+  }
 
     //add more tests here to validate your work
 }
